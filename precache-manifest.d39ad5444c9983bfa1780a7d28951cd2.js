eb8077dad5a4eea5fb46316cef32b91c3938cc0b
akostikov92 <akostikov92@gmail.com>
2020-01-19 18:25:51 -0700

    Updates

---
+++ precache-manifest.d39ad5444c9983bfa1780a7d28951cd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b35f836821ea6acc069f3daa3f5354",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5bc48da4fc2fce6036c9",
+    "url": "/monsters-rolodex/static/css/main.0a2cdce1.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5bc48da4fc2fce6036c9",
+    "url": "/monsters-rolodex/static/js/main.438a27ba.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
